e5b8a3fa630e6ccdce0e644fc3357e318f54ab0f
Evandro2270 <evandro535@gmail.com>
2024-12-14 16:32:37 -0300

    Adicionar arquivos de projeto.

---
+++ TESTE]/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Globalization;
+namespace Programa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+
+                CultureInfo CI = CultureInfo.InvariantCulture;
+
+                int cont = 10;
+
+                do {
+                    Console.WriteLine("Tioze");
+                    cont++; 
+                }
+                while (cont < 5);
+                Console.WriteLine(cont);
+
+            }
+        }
+    }
+}
